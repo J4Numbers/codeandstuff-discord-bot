fba52c859543f1fbe222db7aa3671e8d56c0bdf0
Jayne Doe <j4numbers@gmail.com>
2021-03-17 18:07:36 +0000

    feat(axios): bring in an example axios call
    Signed-off-by: Jayne Doe <j4numbers@gmail.com>

--- src/ts/tickets/eventbrite-event-lookup.ts
+++ src/ts/tickets/eventbrite-event-lookup.ts
@@ -1,26 +1,40 @@
+import * as axios from 'axios';
 import {StandardEventLookup} from "./standard-event-lookup";
 import {Ticket, Tickets} from "../objects/ticket";
 import {Events} from "../objects/event";
 import {NameDetails} from "../objects/name-details";
-import {EventbriteTicketProps} from "../objects/eventbrite-ticket-props";
+import {EventbriteEventProps} from "../objects/eventbrite-event-props";
 
 export class EventbriteEventLookup implements StandardEventLookup {
-  configuration: EventbriteTicketProps;
+  configuration: EventbriteEventProps;
 
   baseEventbriteLink: string = 'https://www.eventbriteapi.com'
   listOrganisationEventsLink: string = 'v3/organizations/ORG_ID/events'
   listAttendeesLink: string = 'v3/events/EVENT_ID/attendees/'
 
-  constructor(props: EventbriteTicketProps) {
+  constructor(props: EventbriteEventProps) {
     this.configuration = props;
+  }
+
+  async performRequest(urlToCall: string): Promise<Object> {
+    return axios(urlToCall);
   }
 
   async getAllActiveEvents(): Promise<Events> {
     const builtLink = `${baseEventbriteLink}/${listOrganisationEventsLink}?token=${this.configuration.token}&status=live`;
-    return [];
+    const filledLink = builtLink.replace('ORG_ID', this.configuration.org_id);
+    try {
+      const eventRequest = await this.performRequest(filledLink);
+      return [];
+    } catch (axiosException) {
+      log.warn(`Unable to get all active events... :: ${axiosException.message}`);
+      throw axiosException;
+    }
   }
 
   async getTicketsAssociatedWithEvent(eventId: string): Promise<Tickets> {
+    const builtLink = `${baseEventbriteLink}/${listAttendeesLink}?token=${this.configuration.token}`;
+    const filledLink = builtLink.replace('EVENT_ID', eventId);
     return [];
   }
 
