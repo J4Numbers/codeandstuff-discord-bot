bb196c5510e45a6bbe6fc1c8aba9dcfdb860f0d9
Jayne Doe <j4numbers@gmail.com>
2021-03-20 21:41:14 +0000

    feat(event): notify of no events on request
    Signed-off-by: Jayne Doe <j4numbers@gmail.com>

--- src/ts/bot/event-promoter.ts
+++ src/ts/bot/event-promoter.ts
@@ -15,21 +15,27 @@ const handleEventCalls = async (incomingMessage: Message) => {
       log.info('Received request to display all current events');
       const currentEvents = await eventManager.getAllActiveEvents();
       log.debug(`Found ${currentEvents.length} current events`);
-      const sendingPromises = currentEvents.map((awaitingEvent) => {
-        const generatedEmbed = new MessageEmbed()
-        .setColor('DARK_ORANGE')
-        .setTitle(awaitingEvent.name.text)
-        .setDescription(awaitingEvent.description.text)
-        .setThumbnail(awaitingEvent.logo.url)
-        .setURL(awaitingEvent.url)
-        .setTimestamp(awaitingEvent.start.utc)
-        .setFooter('Code and Stuff')
-        return incomingMessage.channel.send(
-          'The following is an official Code and Stuff event...',
-          { embed: generatedEmbed },
+      if (currentEvents.length > 0) {
+        const sendingPromises = currentEvents.map((awaitingEvent) => {
+          const generatedEmbed = new MessageEmbed()
+            .setColor('DARK_ORANGE')
+            .setTitle(awaitingEvent.name.text)
+            .setDescription(awaitingEvent.description.text)
+            .setThumbnail(awaitingEvent.logo.url)
+            .setURL(awaitingEvent.url)
+            .setTimestamp(awaitingEvent.start.utc)
+            .setFooter('Code and Stuff');
+          return incomingMessage.channel.send(
+            'The following is an official Code and Stuff event...',
+            { embed: generatedEmbed },
+          );
+        });
+        await Promise.all(sendingPromises);
+      } else {
+        incomingMessage.channel.send(
+          `There are no upcoming official Code and Stuff events...`,
         );
-      });
-      await Promise.all(sendingPromises);
+      }
     } catch (e) {
       log.warn(`Unable to promote upcoming events :: ${e.message}`);
     }
