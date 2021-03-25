af5a8199bdf1a2beaf2e856f428b79591bae3fa9
Jayne Doe <j4numbers@gmail.com>
2021-03-25 05:20:55 +0000

    build(tf): move to using s3 as a backend
    Signed-off-by: Jayne Doe <j4numbers@gmail.com>

--- tf/provider.tf
+++ tf/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket = "uk-j4numbers-dev-terraform-state"
+    key = "cas-discord-bot.tfstate"
+    region = "eu-west-2"
+  }
 }
 
 # Configure the AWS Provider
